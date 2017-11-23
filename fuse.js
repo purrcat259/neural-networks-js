3dc80ec7297b6fa1e5314cfcce380f22b1370ce1
Simon AM <purrcat259@users.noreply.github.com>
2017-11-23 12:23:44 +0100

    Added boilerplate files

---
+++ fuse.js
@@ -0,0 +1,34 @@
+const {
+    FuseBox,
+    HTMLPlugin,
+    WebIndexPlugin,
+    BabelPlugin,
+    SassPlugin,
+    CSSPlugin,
+} = require("fuse-box");
+
+const fuse = FuseBox.init({
+    homeDir: 'src',
+    output: 'dist/$name.js',
+    useTypescriptCompiler: true,
+    sourceMaps: true,
+    target: 'browser',
+    plugins: [
+        WebIndexPlugin({
+            template: './src/index.html'
+        }),
+        [SassPlugin(), CSSPlugin()],
+        BabelPlugin({
+            // limit2project: false
+        })
+    ]
+});
+
+fuse.bundle('client/app')
+    .watch('src/**')
+    .hmr({reload : true})
+    .instructions('> demo.js');
+
+fuse.dev();
+
+fuse.run();
