865cb70d74a965fa8e605cb83e19f61b6bdd0db0
Simon AM <purrcat259@users.noreply.github.com>
2017-11-23 12:55:21 +0100

    Added basic neuron model

---
+++ src/models/neuron.js
@@ -0,0 +1,37 @@
+export default class Neuron {
+    consturctor(layer, inputs, weights, learningRate) {
+        this.layer = layer;
+        this.inputs = inputs;
+        this.weights = weights;
+        this.learningRate = learningRate;
+    }
+
+    calculateNet(inputs, weight) {
+        let net = 0;
+        for (let i = 0; i < inputs.length; i++) {
+            const input = inputs[i];
+            const weight = weights[i];
+            net += (input * weight);
+        }
+        return net;
+    }
+
+    calculateActivation(net) {
+        return 1 / (1 + Math.exp(-net));
+    }
+
+    // Override depending on target / hidden
+    calculateError() {}
+
+    calculateNewWeight(oldWeight, error, inputValue) {
+        return oldWeight + (this.learningRate * error * inputValue);
+    }
+
+    updateWeights(error, target) {
+        for (let i = 0; i < this.weights.length; i++) {
+            let oldWeight = this.weights[i];
+            let inputValue = this.inputs[i];
+            this.weights[i] = this.calculateNewWeight(this.weights[i], error, inputValue);
+        }
+    }
+}
