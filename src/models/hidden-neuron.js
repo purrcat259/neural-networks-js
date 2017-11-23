e3f27a3e37d8de5ca054a3cd824cc167c39b7f83
Simon AM <purrcat259@users.noreply.github.com>
2017-11-23 13:03:46 +0100

    Changed incorrect name

--- src/models/hidden-neuron.js
+++ src/models/hidden-neuron.js
@@ -1,6 +1,6 @@
 import * as Neuron from './neuron';
 
-export default class OutputNeuron extends Neuron {
+export default class HiddenNeuron extends Neuron {
     calculateError(previousError, weight, activation) {
         return previousError * weight * activation * (1 - activation);
     }
